41e855d31c47cb10c1f166463aee50319aa80122
Nwakanma Emmanuel <nwakanmaemmanuel60@gmail.com>
2026-03-03 17:19:51 +0100

    overlay and modal added

--- src/components/NoteViewer.jsx
+++ src/components/NoteViewer.jsx
@@ -9,13 +9,17 @@ export default function NoteViewer( {note ,setSelectedNote,editingNoteId, handle
 
     return (
         // adding modal class
-        <div className='absolute inset-0 bg-linear-to-br from-[#763ea0] via-[#571788] to-[#331b58] border border-[#754797]  text-amber-50 left-[20%] top-[30%] p-6 rounded-[10px] w-[510px] h-[211px] flex flex-col gap-2.5'>
+        <div
+            onClick={() => setSelectedNote(null)}
+            className="fixed inset-0 bg-black/40 flex items-center justify-center">
+        <div className=' bg-linear-to-br from-[#763ea0] via-[#571788] to-[#331b58] border border-[#754797]  text-amber-50 left-[20%] top-[30%] p-6 rounded-[10px] w-[510px] h-[211px] flex flex-col gap-2.5'
+            onClick={(e) => e.stopPropagation()} >
 
             
                 <div key={note.id}>
                 <div  className='flex justify-between mb-2.5'>
                     <h1 className='text-3xl font-bold' >{note.title || "Untitled Note"}</h1>
-                    <button onClick={() => setSelectedNote(false)} className=' absolute left-[482px] text-[17px] font-bold text-lg  top-0 hover:scale-[1.3] '>&times;</button>
+                    <button onClick={() => setSelectedNote(null)} className=' absolute left-[482px] text-[17px] font-bold text-lg  top-0 hover:scale-[1.3] '>&times;</button>
                     <div  className='flex'>
                         <button onClick={() => handleEditClick(note)}  className='flex gap-2 items-center px-4 border border-[#44609e] rounded-md cursor-auto text-[#a1adca] bg-[#3f4773] hover:bg-[#414f9b] ' >
                             <i className="fa-regular fa-pen-to-square text-[13px] font-extrabold  hover:text-black"></i>               
@@ -53,6 +57,7 @@ export default function NoteViewer( {note ,setSelectedNote,editingNoteId, handle
             
             
             
+        </div>
         </div>
             
         
