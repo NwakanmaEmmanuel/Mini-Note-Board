37f085bf40a41235e318e9abd0d0a31b5bb066ec
Nwakanma Emmanuel <nwakanmaemmanuel60@gmail.com>
2026-03-31 22:01:56 +0100

    styled the foldertab section

--- src/components/FolderTab.jsx
+++ src/components/FolderTab.jsx
@@ -3,43 +3,26 @@ import NoteCard from "./NoteCard"
 
 export default function FolderTab({ folder,folders,setShowBox,showBox, addNewNote, handleDeleteNote, handleUpdateNote, setSelectedFolder, notes }) {
 
-  //     function handleCreateNote(e) {
-  // e.preventDefault();
-  //   // const createdAt = new Date()
-  //   const now = new Date().toISOString()
-
-  //   addNewNote({
-  //     id: Date.now(),
-  //     title,
-  //     content,
-  //     createdAt: now,
-  //     updatedAt: now
-  //   })
-
-  //     setShowBox(false);
-    
-  // }
   const folderNotes = notes.filter(note => Number(note.folderId) === Number(folder.id))
   
 
   return (
     // <div className="p-14 bg-red-600 shadow-xl   rounded-3xl border border-[#4b455c] m-10 flex flex-col">
+    <>
 
-      <div className="p-14 bg-linear-to-br  via-purple-900 to-slate-900  shadow-xl backdrop-blur-lgp-10 w-120 rounded-3xl border border-[#4b455c] m-[71px] items-center flex flex-col justify-center">
-      
       {folderNotes.length === 0 ? 
-      <>
-         <div className="text-7xl mb-6 relative ">📁</div>
-        <h1 className="text-4xl font-bold text-white mb-4.5">Empty Folder</h1>
-        <p className="font-medium text-xl text-center break-all text-[#f3e9ff] mb-5.5">
-          <span className="text-[#8abbf5] ">"{folder.name}"</span> has no notes yet</p>
-        <button onClick={() => setShowBox(true)} className=" group p-3 mb-[17px] bg-blue-500 text-white text-xl font-bold rounded-[18px] transform transition hover:scale-105 active:scale-95 bg-linear-to-r from-[#9641dd] via-[#5465da] to-[#078eb2]  ">
-            <span className="mr-[15px] inline-block">+</span> Create Your First Note <span className="text-white ml-[15px]">✨</span>
-        </button>
-        {showBox &&  <NewNote folders={folders} currentFolderId={folder.id} setShowBox={setShowBox} addNewNote={addNewNote} setSelectedFolder={setSelectedFolder} />}
         
-      </> : ( 
-          <NoteCard
+        <div className="p-14 bg-linear-to-br  via-purple-900 to-slate-900  shadow-xl backdrop-blur-lgp-10 w-120 rounded-3xl border border-[#4b455c] m-[71px] items-center flex flex-col justify-center">
+          <div className="text-7xl mb-6 relative ">📁</div>
+          <h1 className="text-4xl font-bold text-white mb-4.5">Empty Folder</h1>
+          <p className="font-medium text-xl text-center break-all text-[#f3e9ff] mb-5.5">
+            <span className="text-[#8abbf5] ">"{folder.name}"</span> has no notes yet</p>
+          <button onClick={() => setShowBox(true)} className=" group p-3 mb-[17px] bg-blue-500 text-white text-xl font-bold rounded-[18px] transform transition hover:scale-105 active:scale-95 bg-linear-to-r from-[#9641dd] via-[#5465da] to-[#078eb2]  ">
+              <span className="mr-[15px] inline-block">+</span> Create Your First Note <span className="text-white ml-[15px]">✨</span>
+          </button>
+          {showBox &&  <NewNote folders={folders} currentFolderId={folder.id} setShowBox={setShowBox} addNewNote={addNewNote} setSelectedFolder={setSelectedFolder} />}
+        </div> :  
+        <NoteCard
             notes={folderNotes}
             folders={folders}
             setShowBox={setShowBox}
@@ -47,14 +30,10 @@ export default function FolderTab({ folder,folders,setShowBox,showBox, addNewNot
             addNewNote={addNewNote}
             handleDeleteNote={handleDeleteNote}
             handleUpdateNote={handleUpdateNote}
-          />
-        // folderNotes.map(note => (
-        //   <div key={note.id} className="text-white">
-        //     {note.title}
-        //   </div>
-        // ))
-      )}
-       
-       </div>
+          />}
+
+      
+        </>
+     
   )
 }
